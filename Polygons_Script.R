6d71a866faf57c98425257894cd5a60b58e20a1a
Yohay287 <71830197+Yohay287@users.noreply.github.com>
2020-10-08 19:29:32 +0300

    Add files via upload

--- Polygons_Script.R
+++ Polygons_Script.R
@@ -1,11 +1,10 @@
 
 library(utils)
+library(sp)
 library(maptools)
-library(SDMTools)
 
 KML_Folder <- " SET THE DIRECTORY WITH ALL THE POLYGONS FILES IN KML FORMAT " ## Set Directory with Polygons Files
 Locations_Data <- read.csv(" SET THE PATH TO YOUR DATA ") ## Import Locations Data with Latitude & Longitude Columns
-
 
 kmlfiles <- list.files(KML_Folder, pattern=".kml", full.names=TRUE) ## importing list of KMLs files in folder
 kmlnames <- list.files(KML_Folder, pattern=".kml") ## create list of file names
@@ -21,11 +20,12 @@ for (k in 1:length(kmlfiles)) {
   
   #assign(kmlnames[[k]],tmp_kml) ### ENABLE THIS TO SAVE POLYGONS AS ELEMENTS ###
   
-  in_poly_tmp <- pnt.in.poly(latlon, tmp_kml) ## Check if points in polygon
-  Locations_Data$Polygon[which(in_poly_tmp$pip==1)] <- kmlnames[[k]]
+  in_poly_tmp <- point.in.polygon(point.x = latlon$Longitude, ## Check if points in polygon
+                                  point.y = latlon$Latitude,
+                                  pol.x = tmp_kml$Longitude,
+                                  pol.y = tmp_kml$Latitude)
+  Locations_Data$Polygon[which(in_poly_tmp==1)] <- kmlnames[[k]]
   rm(in_poly_tmp)
   rm(tmp_kml)
   
 }
-
-
